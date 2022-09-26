9291676f173d1b071d32d4a9208853769611ff4c
superkacper4 <kucza06@wp.pl>
2022-09-27 00:17:51 +0200

    add logout to nav

--- app/components/Nav/Nav.tsx
+++ app/components/Nav/Nav.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@remix-run/react"
+import { Form, Link } from "@remix-run/react"
 import { useOptionalUser } from "~/utils";
 
 const Nav = () => {
@@ -9,12 +9,23 @@ const Nav = () => {
             <p>Version System Control of Documentation</p>
             <div className="mx-auto mt-10 max-w-sm sm:flex sm:max-w-none sm:justify-center">
                 {user ? (
+                  <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
+
                   <Link
                     to="/notes"
                     className="flex items-center justify-center rounded-md border border-transparent bg-white px-4 py-3 text-base font-medium text-yellow-700 shadow-sm hover:bg-yellow-50 sm:px-8"
                   >
                     View Notes for {user.email}
                   </Link>
+                  <Form action="/logout" method="post">
+                        <button
+                        type="submit"
+                        className="rounded bg-slate-600 py-2 px-4 text-blue-100 hover:bg-blue-500 active:bg-blue-600"
+                        >
+                        Logout
+                        </button>
+                    </Form>
+                  </div>
                 ) : (
                   <div className="space-y-4 sm:mx-auto sm:inline-grid sm:grid-cols-2 sm:gap-5 sm:space-y-0">
                     <Link
